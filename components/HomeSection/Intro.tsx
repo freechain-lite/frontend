9942bc019c62c2dbc941e59c7b2452d925528c36
Udoagwa Franklin <frankudoags@gmail.com>
2024-02-03 13:38:04 +0100

    Initial commit

---
+++ components/HomeSection/Intro.tsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Intro = () => {
+    return (
+        <main>
+            <div className="relative isolate px-6 lg:px-8">
+                <div
+                    className="absolute inset-x-0 -z-40 transform-gpu overflow-hidden blur-3xl"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
+                </div>
+                <div className="mx-auto max-w-5xl py-20 sm:py-28 lg:py-32">
+                    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+                        <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                            Are you eligble for Freechain Airdrop?{' '}
+                            <a href="#" className="font-semibold text-indigo-600">
+                                <span className="absolute inset-0" aria-hidden="true" />
+                                Claim your Pass now <span aria-hidden="true">&rarr;</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold tracking-tight text-gray-700 sm:text-6xl lg:text-7xl">
+                            <span className='text-gradient bg-clip-text text-transparent bg-gradient-to-r to-blue-800 from-indigo-600'>
+                                Freechain: {" "}
+                            </span>
+                            Trade, create, explore
+                        </h1>
+                        <p className="mt-6 text-gray-600">
+                            Step into a world where trading NFTs knows no bounds. Our intuitive platform empowers you to engage in transactions with unparalleled simplicity. Whether you&apos;re a seasoned collector or a newcomer to the NFT scene, the power to trade is now at your fingertips.
+                        </p>
+                        <div className="mt-10 flex items-center justify-center gap-x-6">
+                            <Link
+                                href="/"
+                                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                            >
+                                Get started
+                            </Link>
+                            <Link href="/explore/collections" className="text-sm font-semibold leading-6 text-gray-900">
+                                Explore Collections <span aria-hidden="true">→</span>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+                    aria-hidden="true"
+                >
+                    <div
+                        className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+                        style={{
+                            clipPath:
+                                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                        }}
+                    />
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Intro
---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import { Stack } from '@chakra-ui/react'
+import AssetsHomeSection from 'components/HomeSection/Assets'
+import CollectionsHomeSection from 'components/HomeSection/Collections'
+import FeaturedHomeSection from 'components/HomeSection/Featured'
+import ResourcesHomeSection from 'components/HomeSection/Resources'
+import UsersHomeSection from 'components/HomeSection/Users'
+import { NextPage } from 'next'
+import { useCallback, useState } from 'react'
+import Head from '../components/Head'
+import useCart from '../hooks/useCart'
+import LargeLayout from '../layouts/large'
+import Intro from 'components/HomeSection/Intro'
+
+type Props = {
+  now: number
+}
+const HomePage: NextPage<Props> = ({ now }) => {
+  const [date, setDate] = useState(new Date(now))
+  const updateDate = useCallback(() => setDate(new Date()), [])
+  useCart({ onCheckout: updateDate })
+  return (
+    <LargeLayout>
+      <Head />
+      <Stack spacing={32}>
+        <Intro />
+        <FeaturedHomeSection date={date} />
+        <CollectionsHomeSection />
+        <UsersHomeSection />
+        <AssetsHomeSection date={date} />
+        <ResourcesHomeSection />
+      </Stack>
+    </LargeLayout>
+  )
+}
+
+export default HomePage
